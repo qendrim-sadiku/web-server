5525d75aa8220e6091c9b6ee04bac780f30a1178
Qendrim Sadiku <qsadiku867@gmail.com>
2024-08-12 09:42:09 +0200

    change
    changes

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const serviceRoutes = require('./routes/services/servicesRoutes');
 const bookingRoutes = require('./routes/bookings/bookingRoutes');
 const trainerRoutes = require('./routes/trainer/trainerRoutes');
 
-const sequelize = require('./config/sequelize');
+const sequelize = require('./config/sequelize'); // Import your Sequelize instance
 const cors = require('cors');
-const locationRoutes = require('./routes/locationRoutes')
+const locationRoutes = require('./routes/locationRoutes');
 const fs = require('fs');
 const path = require('path');
 
-
-
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // Ensure the uploads directory exists
 const uploadDir = path.join(__dirname, 'uploads');
@@ -28,26 +26,33 @@ if (!fs.existsSync(uploadDir)) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-// sequelize.sync({ force: true }); // This will drop existing tables and recreate them
+
+// Test the Sequelize connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // Routes
 app.use('/auth', authRoutes);
 app.use('/api/', userRoutes); // Mount user routes
-app.use('/countries', locationRoutes); // Mount user routes
-// Use the trainer routes
-
-
+app.use('/countries', locationRoutes); // Mount location routes
 app.use('/api/services', serviceRoutes);
 app.use('/api/', bookingRoutes);
 app.use('/api/trainers', trainerRoutes);
-
-
 app.use('/api', categoryRoutes);
 
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-  
+// Root route to send a message when visiting the app
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express App!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
