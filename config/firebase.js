694e7fb901c057efb45a47a539bd65cff5fde62b
Qendrim Sadiku <qsadiku867@gmail.com>
2024-10-28 14:17:06 +0100

    changes
    changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,27 @@
-var admin = require("firebase-admin");
+// config/firebaseAdmin.js
+const admin = require('firebase-admin');
+const dotenv = require('dotenv');
 
-var serviceAccount = require("./aroit-firebase.json");
+// Load environment variables from the .env file
+dotenv.config();
 
+// Construct the service account object using environment variables
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
+
+// Initialize the Firebase Admin SDK using the service account object
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
+
+module.exports = admin;
