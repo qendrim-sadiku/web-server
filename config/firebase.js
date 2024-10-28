1d58f671191cab372af149923ac63de8fc62d5fc
Qendrim Sadiku <qsadiku867@gmail.com>
2024-10-28 14:32:30 +0100

    changes
    changes

--- config/firebase.js
+++ config/firebase.js
@@ -17,7 +17,7 @@ const serviceAccount = {
   token_uri: process.env.FIREBASE_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+}; 
 
 // Initialize the Firebase Admin SDK using the service account object
 admin.initializeApp({
