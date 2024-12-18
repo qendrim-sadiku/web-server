4651a7a87346d086a6657deedf12d4a66fa334d1
Qendrim Sadiku <qsadiku867@gmail.com>
2024-12-18 16:09:17 +0100

    changes
    changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ const authController = require('../controllers/authController');
 const passport = require('../config/passport');
 
 
-const FRONTEND_URL =  'https://aroit.com';
+const FRONTEND_URL = 'https://aroit.com';
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
